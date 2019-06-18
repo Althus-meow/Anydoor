3bd6bc2fa63a394b1b47bce3e248195176e0ef70
Althus Zhou <althus_@hotmail.com>
2019-06-18 20:40:09 +0800

    Prototype v0.3, dynamic template and file content type

---
+++ src/helper/mime.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const mimeTypes = require("../config/mimeTypes");
+
+module.exports = function(filePath){
+  let extName = path.extname(filePath).split(".").pop().toLowerCase;
+
+  if (!extName){
+    extName = filePath;
+  }
+
+  return mimeTypes[extName] || mimeTypes["txt"];
+};
--- src/helper/route.js
+++ src/helper/route.js
@@ -1,25 +1,39 @@
 const fs = require("fs");
 const path = require("path");
 const basicConfig = require("../config/basicConfig");
+const handlebars = require("handlebars");
+const mime = require("../helper/mime");
 
 const { promisify } = require("util");
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 // const createReadStream = promisify(fs.createReadStream);
 
+const dirTplPath = path.join(__dirname, "../templates/dir.tpl");
+const dirTpl = handlebars.compile(fs.readFileSync(dirTplPath, "utf-8"));
+
 module.exports = async function (req, res) {
   const filePath = path.join(basicConfig.root, req.url);
   try {
     const stats = await stat(filePath);
     if (stats.isFile()) {
+      const contentType = mime(filePath);
       res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
+      res.setHeader("Content-Type", contentType);
       fs.createReadStream(filePath).pipe(res);
     } else if (stats.isDirectory()) {
       let files = await readdir(filePath);
+      const dir = path.relative(basicConfig.root, filePath);
+      const pageData = {
+        files,
+        dir : dir? `/${dir}` : "",
+        title : path.basename(filePath)
+      };
+
       res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.end(files.join(", "));
+      res.setHeader("Content-Type", "text/html");
+      // res.end(files.join(", "));
+      res.end(dirTpl(pageData));
     }
   } catch (err) {
     res.statusCode = 404;
