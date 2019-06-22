1bb0b2a49a2f0509c6bd201b8b9653cabd807bb8
Althus Zhou <althus_@hotmail.com>
2019-06-22 16:37:00 +0800

    ver 1.1.0, added function to open url automatically

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const http = require("http");
 const basicConfig = require("./config/basicConfig");
 const route = require("./helper/route");
+const openUrl = require("./helper/openUrl");
 
 class MyServer {
 
@@ -17,6 +18,7 @@ class MyServer {
     server.listen(this.config.port, this.config.hostname, () => {
       const addr = `http://${this.config.hostname}:${this.config.port}`;
       console.log(`Server start at ${addr}`);
+      openUrl(addr);
     });
   }
 }
